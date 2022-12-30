b92247affb5bb6a2c58f95914f7e3cef97049511
JNKarif <arif.rahman041@gmail.com>
2022-12-30 21:25:45 +0600

    new-api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors =require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 const app = express()
@@ -11,15 +12,39 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.ei4prfy.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ei4prfy.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+
+  try{
+    const tasksListCollection = client.db('doTheTask').collection('tasksList')
+
+   
+
+  app.post('/taskslist', async(req, res)=>{
+    const tasks = req.body
+    console.log(tasks);
+    const result = await tasksListCollection.insertOne(tasks)
+    res.send(result)
+  })
 
 
+  app.get('/taskslist', async (req, res) => {
+    const query = {};
+    const task = await tasksListCollection.find(query).toArray();
+    res.send(task)
+})
+
+
+  }
+  finally{
+
+  }
+}
+
+run().catch(console.log)
 
 app.get('/', async(req, res)=>{
     res.send('do the task server is runnig')
